4e16297e1bf04ea24c476a7d2d90581ad0488f22
disrael <disrael@gmail.com>
2023-02-20 17:39:39 -0800

    Updates

---
+++ precache-manifest.b96c8e4c1e8b3267fc9fa3a33b7ef00d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18d759f2dbacf5d817fde6c016a442d7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "91b3932c089eb1f580ae",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "353b7ca4d32911afc368",
+    "url": "./static/js/2.2e8b48cf.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.2e8b48cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91b3932c089eb1f580ae",
+    "url": "./static/js/main.30f22ce9.chunk.js"
+  },
+  {
+    "revision": "9b9f4e0eb5c50ad3c786",
+    "url": "./static/js/runtime-main.acdc226c.js"
+  }
+]);
